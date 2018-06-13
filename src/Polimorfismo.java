0f0fdbc1c02b21e52890b2cf74454dd58e51f10c
cice <cice@A8POVMac11.local>
2018-06-13 20:28:01 +0200

    polimorfismo

--- src/Polimorfismo.java
+++ src/Polimorfismo.java
@@ -1,2 +1,22 @@
+
+
+import java.util.Scanner;
+
 public class Polimorfismo {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce primer numero: ");
+        double a = sc.nextDouble();
+        System.out.println("Introduce segundo numero: ");
+        double b = sc.nextDouble();
+
+        Calculadora c = new Calculadora(a,b);
+
+        System.out.println(c.suma());
+
+    }
+
 }
